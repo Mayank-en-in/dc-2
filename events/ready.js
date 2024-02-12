e919ab37ff41db46eb4245187f364f951af8ab0f
Mayank Sharma <118103735+Mayank-en-in@users.noreply.github.com>
2024-02-12 22:26:55 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,11 +21,18 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-setInterval(() => client.user.setActivity({ 
-  name: `type /help For Music`, 
-  type: 'STREAMING',  // Set activity type to 'STREAMING'
-  url: 'http://twitch.tv/streamer'  // Replace with your Twitch or YouTube URL
-}), 10000);
+setInterval(() => {
+  try {
+    client.user.setActivity({ 
+      name: `type /help For Music`, 
+      type: 'STREAMING',
+      url: 'http://twitch.tv/streamer'
+    });
+    console.log('Activity set successfully!');
+  } catch (error) {
+    console.error('Error setting activity:', error);
+  }
+}, 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
