6fa5b4b2306aae690cd4f7c98481813a384b9eea
Mayank Sharma <118103735+Mayank-en-in@users.noreply.github.com>
2024-02-12 19:21:02 +0530

    Add files via upload

---
+++ commands/owner.js
@@ -0,0 +1,57 @@
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
+const { EmbedBuilder } = require('discord.js');
+const db = require("../mongoDB");
+
+module.exports = {
+  name: "owner",
+  description: "Get information about bot owner.",
+  permissions: "0x0000000000000800",
+  options: [],
+
+  run: async (client, interaction) => {
+    try {
+      const { EmbedBuilder } = require('discord.js');
+      const embed = new EmbedBuilder()
+        .setColor('#da2a41')
+        .setAuthor({
+          name: 'Physics',
+          iconURL: 'https://wallpapercave.com/wp/wp1904054.png',
+        })
+        .setDescription(`__**About me**__:\n\n ▶️ Phy.sics. I am a discord bot developer and web developer. I love playing games, watching anime and building different web server applications. You will get faster replies on Instagram than other social media. Feel free to contact me!`)
+        .setTimestamp();
+      
+      interaction.reply({ embeds: [embed] }).catch(e => {});
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
